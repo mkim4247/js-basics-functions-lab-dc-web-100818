2eaf40a6e40e89b3b0abe0da0f1d7087c583264a
Michael Kim <m.kim4247@gmail.com>
2018-11-17 12:57:57 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(block) {
+  if (block > 42) {
+    return (block - 42);
+  }
+  else if (block < 42){
+    return (42 - block);
+  }
+}
+
+function distanceFromHqInFeet(block) {
+  return (distanceFromHqInBlocks(block) * 264);
+}
+
+function distanceTravelledInFeet(start, end) {
+  if (start > end) {
+    return ((start - end) * 264);
+  }
+  else if (start < end) {
+    return ((end - start) * 264);
+  }
+}
+
+function calculatesFarePrice(start, end) {
+  let distance = distanceTravelledInFeet(start, end);
+
+  if (distance < 400) {
+    return 0
+  }
+  else if (distance > 400 && distance < 2000) {
+    return ((distance - 400) * 0.02)
+  }
+  else if (distance > 2000 && distance < 2500) {
+    return 25
+  }
+  else {
+    return 'cannot travel that far'
+  }
+}
